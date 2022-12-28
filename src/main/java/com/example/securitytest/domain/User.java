8d964f819141ebd6dc6e9013554b5ff662b260f6
bookgvi <bookgvi@gmail.com>
2022-12-28 23:07:04 +0300

    git init

---
+++ src/main/java/com/example/securitytest/domain/User.java
@@ -0,0 +1,22 @@
+package com.example.securitytest.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USERS")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "user_name")
+    private String userName;
+    @Column
+    private String password;
+}
